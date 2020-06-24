5d542889750032fedca16546de8f315c123f7eaf
jellyxie <821726220@qq.com>
2020-06-24 08:17:42 +0800

    initial MainVC

---
+++ GitDemo/GitDemo/ViewController/XGDMainViewController.m
@@ -0,0 +1,27 @@
+//
+//  XGDMainViewController.m
+//  GitDemo
+//
+//  Created by 谢果冻 on 2018/8/5.
+//  Copyright © 2018年 北京心花怒放网络科技有限公司. All rights reserved.
+//
+
+#import "XGDMainViewController.h"
+
+@interface XGDMainViewController ()
+
+@end
+
+@implementation XGDMainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
